befafa283d4449448614689d6c5d8ca7919b8bdd
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-06-22 00:07:12 +0000

    Nuevos cambios

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const propTypes = require("prop-types");
-
 const BootstrapCard = () => 
 {
     return <div className="card m-5">
@@ -9,7 +7,7 @@ const BootstrapCard = () =>
         <div className="card-body">
             <h5 className="card-title"></h5>
             <p className="card-text">
-                
+                Hello world
             </p>
             <a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
                 Go to wikipedia
