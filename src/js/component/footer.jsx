d83d808587d907bf3fa8fac2a9cec8fdf7c53a14
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-06-22 00:26:29 +0000

    Cambios

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,15 +1,15 @@
 const Footer = () => {
-    return "Footer";
+    return <div className="card">
+    <div className="card-header">
+      Featured
+    </div>
+    <div className="card-body">
+      <h5 className="card-title">Special title treatment</h5>
+      <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>;
   };
   export default Footer;
 
-  /*<div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>*/
+  
